f9a6e7937a44bdc3eddc950c7725e55adf69ea78
zyrouge <naveencnaveen96@gmail.com>
2020-06-20 18:07:55 +0530

    Added Rankcard

--- build/assets/js/recommendations.js
+++ build/assets/js/recommendations.js
@@ -60,6 +60,16 @@ $(async function () {
             );
         }
     }
+    $(".image-button").click(function () {
+        var target = $(this).data("target");
+        $("html").addClass("is-clipped");
+        $(target).addClass("is-active");
+    });
+
+    $(".modal-close").click(function () {
+        $("html").removeClass("is-clipped");
+        $(this).parent().removeClass("is-active");
+    });
 });
 
 function recommendationsBoxHTML (recommendationsBox, routes) {
